a60a9d47cac456414e9d06f9ea4a725b999a6e18
Oroko <aoroko09@gmail.com>
2021-05-14 22:58:07 -0400

    Added a gem to reset pk sequence, also did a migration to a remove foreign key and use the references to add an indexed search on the table with the fk

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_15_010922) do
+ActiveRecord::Schema.define(version: 2021_05_15_022922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,9 +27,11 @@ ActiveRecord::Schema.define(version: 2021_05_15_010922) do
     t.string "genre"
     t.string "imgUrl"
     t.string "status"
-    t.integer "author_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
+  add_foreign_key "books", "authors"
 end
